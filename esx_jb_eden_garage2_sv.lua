b120784a0ee56022c0b63e466f796b0230a09475
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2019-06-16 17:43:02 +0200

    Update esx_jb_eden_garage2_sv.lua

--- esx_jb_eden_garage2_sv.lua
+++ esx_jb_eden_garage2_sv.lua
@@ -16,7 +16,8 @@ ESX.RegisterServerCallback('eden_garage:getVehicles', function(source, cb, KindO
 
 	MySQL.Async.fetchAll("SELECT * FROM owned_vehicles WHERE owner=@identifier",{['@identifier'] = identifier}, function(data) 
 		for _,v in pairs(data) do
-			table.insert(vehicules, {vehicle = v.vehicle, state = v.state, fourrieremecano = v.fourrieremecano, plate = v.plate, vehiclename = v.vehiclename})
+			local plate = ESX.Math.Trim(v.plate)
+			table.insert(vehicules, {vehicle = v.vehicle, state = v.state, fourrieremecano = v.fourrieremecano, plate = plate, vehiclename = v.vehiclename})
 		end
 		cb(vehicules)
 	end)
@@ -30,7 +31,8 @@ ESX.RegisterServerCallback('eden_garage:getVehiclesMecano', function(source, cb)
 
 	MySQL.Async.fetchAll("select * from owned_vehicles inner join characters on owned_vehicles.owner = characters.identifier where fourrieremecano=@fourrieremecano",{['@fourrieremecano'] = true}, function(data) 
 		for _,v in pairs(data) do
-			table.insert(vehicules, {vehicle = v.vehicle, state = v.state, fourrieremecano = v.fourrieremecano, firstname = v.firstname, lastname = v.lastname, plate = v.plate})
+			local plate = ESX.Math.Trim(v.plate)
+			table.insert(vehicules, {vehicle = v.vehicle, state = v.state, fourrieremecano = v.fourrieremecano, firstname = v.firstname, lastname = v.lastname, plate = plate})
 		end
 		cb(vehicules)
 	end)
@@ -46,7 +48,7 @@ ESX.RegisterServerCallback('eden_garage:stockv',function(source,cb, vehicleProps
 	else
 		identifier = GetPlayerIdentifiers(_source)[1]
 	end
-	local vehplate = vehicleProps.plate:match("^%s*(.-)%s*$")
+	local vehplate = ESX.Math.Trim(vehicleProps.plate)
 	local vehiclemodel = vehicleProps.model
 	MySQL.Async.fetchAll("SELECT * FROM owned_vehicles where plate=@plate and owner=@identifier",{['@plate'] = vehplate, ['@identifier'] = identifier}, function(result)  
 		if result[1] ~= nil then
@@ -69,7 +71,7 @@ end)
 
 ESX.RegisterServerCallback('eden_garage:stockvmecano',function(source,cb, vehicleProps)
 	local _source = source
-	local plate = vehicleProps.plate:match("^%s*(.-)%s*$")
+	local plate = ESX.Math.Trim(vehicleProps.plate)
 	local vehiclemodel = vehicleProps.model
 	local identifier = GetPlayerIdentifiers(_source)[1]
 	MySQL.Async.fetchAll("SELECT * FROM owned_vehicles where plate=@plate",{['@plate'] = plate}, function(result) 
@@ -93,14 +95,15 @@ end)
 --Change le state du véhicule
 RegisterServerEvent('eden_garage:modifystate')
 AddEventHandler('eden_garage:modifystate', function(plate, state)
-	MySQL.Sync.execute("UPDATE owned_vehicles SET state =@state WHERE plate=@plate",{['@state'] = state , ['@plate'] = plate:match("^%s*(.-)%s*$")})
+	local plate = ESX.Math.Trim(plate)
+	MySQL.Sync.execute("UPDATE owned_vehicles SET state =@state WHERE plate=@plate",{['@state'] = state , ['@plate'] = plate})
 end)	
 --Fin change le state du véhicule
 
 RegisterServerEvent('eden_garage:ChangeStateFromFourriereMecano')
 AddEventHandler('eden_garage:ChangeStateFromFourriereMecano', function(vehicleProps, fourrieremecano)
 	local _source = source
-	local vehicleplate = vehicleProps.plate:match("^%s*(.-)%s*$")
+	local vehicleplate = ESX.Math.Trim(vehicleProps.plate)
 	local fourrieremecano = fourrieremecano
 	
 	MySQL.Sync.execute("UPDATE owned_vehicles SET fourrieremecano =@fourrieremecano WHERE plate=@plate",{['@fourrieremecano'] = fourrieremecano , ['@plate'] = vehicleplate})
@@ -109,7 +112,8 @@ end)
 
 RegisterServerEvent('eden_garage:renamevehicle')
 AddEventHandler('eden_garage:renamevehicle', function(vehicleplate, name)
-	MySQL.Sync.execute("UPDATE owned_vehicles SET vehiclename =@vehiclename WHERE plate=@plate",{['@vehiclename'] = name , ['@plate'] = vehicleplate:match("^%s*(.-)%s*$")})
+	local vehicleplate = ESX.Math.Trim(vehicleplate)
+	MySQL.Sync.execute("UPDATE owned_vehicles SET vehiclename =@vehiclename WHERE plate=@plate",{['@vehiclename'] = name , ['@plate'] = vehicleplate})
 end)
 
 ESX.RegisterServerCallback('eden_garage:getOutVehicles',function(source, cb, KindOfVehicle)	
