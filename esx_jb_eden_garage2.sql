3070e3213e5b616db58b7999adfac43b4a9e31de
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2019-08-25 18:10:46 +0200

    Delete esx_jb_eden_garage2.sql

--- esx_jb_eden_garage2.sql
+++
@@ -1,8 +0,0 @@
-USE `essentialmode`;
-
-ALTER TABLE `owned_vehicles`
-	ADD `state` BOOLEAN NOT NULL DEFAULT FALSE COMMENT 'Etat de la voiture' AFTER `owner`,
-	ADD `fourrieremecano` BOOLEAN NOT NULL DEFAULT FALSE,
-	ADD `vehiclename` varchar(50) NOT NULL DEFAULT 'voiture',
-	ADD INDEX `index_owned_vehicles_owner` (`owner`)
-;
