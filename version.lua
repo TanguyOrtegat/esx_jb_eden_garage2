1e47cd8f778d8b602e4e87447ebd1bc09be0a663
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2019-11-30 15:38:06 +0100

    increment version

--- version.lua
+++ version.lua
@@ -1,5 +1,5 @@
 --[[ Version Checker ]]--
-local VERSION = "3.1.0"
+local VERSION = "3.1.1"
 
 AddEventHandler("onResourceStart", function(resource)
     if resource == GetCurrentResourceName() then
