419a215552bf22bee8be292f58ccaf3460901f83
Joshua <github@itzcrutchie.com>
2020-01-22 04:32:27 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'adamant'
+
+game 'gta5
+
+description 'ESX JB Eden Garage'
+
+dependencies {
+	'es_extended',
+	'ft_libs'
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'server/main.lua',
+	'version.lua',
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'client/main.lua',
+}
